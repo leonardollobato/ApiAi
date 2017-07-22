64749ec0e6eff7b7fec9887657dfaa29d352c547
Leonardo Lobato <leonardolobato@gmail.com>
2017-07-22 17:33:38 -0300

    update

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/json', function(request, response) {
+  response.json({"foo": "bar"});
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
