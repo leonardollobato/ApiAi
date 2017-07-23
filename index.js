63c8b6ed2560c1e82788bed6b7c1ecd671665aa0
Leonardo Lobato <leonardolobato@gmail.com>
2017-07-23 14:18:10 -0300

    update

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.post('/json', function (request, response) {
 });
 
 app.post('/tempoWebhook', function (request, response) {
-  var city = request.body.result.parameters['geo-city'];
+  // var city = request.body.result.parameters['geo-city'];
   var date = '';
-  response.json({ 'city': city });
+  response.json({ 'city': 'city' });
   // if (request.body.result.parameters['date']) {
   //   date = request.body.result.parameters['date'];
   //   console.log('Date: ' + date);
@@ -39,41 +39,6 @@ app.post('/tempoWebhook', function (request, response) {
   //   response.json(JSON.stringify({ 'speech': error, 'displayText': error }));
   // });
 });
-
-// function callWeatherApi(city, date) {
-//   return new Promise((resolve, reject) => {
-//     // Create the path for the HTTP request to get the weather
-//     let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-//       '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
-//     console.log('API Request: ' + host + path);
-//     // Make the HTTP request to get the weather
-//     http.get({ host: host, path: path }, (res) => {
-//       let body = ''; // var to store the response chunks
-//       res.on('data', (d) => { body += d; }); // store each response chunk
-//       res.on('end', () => {
-//         // After all the data has been received parse the JSON for desired data
-//         let response = JSON.parse(body);
-//         let forecast = response['data']['weather'][0];
-//         let location = response['data']['request'][0];
-//         let conditions = response['data']['current_condition'][0];
-//         let currentConditions = conditions['weatherDesc'][0]['value'];
-//         // Create response
-//         let output = `Current conditions in the ${location['type']} 
-//         ${location['query']} are ${currentConditions} with a projected high of
-//         ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
-//         ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
-//         ${forecast['date']}.`;
-//         // Resolve the promise with the output text
-//         console.log(output);
-//         resolve(output);
-//       });
-//       res.on('error', (error) => {
-//         reject(error);
-//       });
-//     });
-//   });
-// }
-
 
 app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
